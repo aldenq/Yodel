484e6b3eb384108c29db42f81f4d0d0bb46992d2
Philo Kaulkin <48894630+Phil0nator@users.noreply.github.com>
2021-03-08 18:09:26 -0800

    Extra typehints and reformatting
    - Typehints in globaldat, and dynamicheaders.py
    - Constructors moved to the tops of classes in dynamicheaders.py
    - Removed extra commented code in dynamicheaders.py

--- standardformats.py
+++ standardformats.py
@@ -1,24 +1,21 @@
 from yodel.dynamicheaders import *
 
 
-standard_header = [ #begins all messages
-    Field("mid",int,bytes=4), #message id
-    Field("Rname",str,bytes=255), #receiver name
-    Field("Gname",str,bytes = 255), #group identifier
-    Field("Sname",str,bytes = 255), #sender name
-    Field("mtype",int,bytes = 1)  #message type 
+standard_header = [  # begins all messages
+    Field("mid", int, bytes=4),  # message id
+    Field("Rname", str, bytes=255),  # receiver name
+    Field("Gname", str, bytes=255),  # group identifier
+    Field("Sname", str, bytes=255),  # sender name
+    Field("mtype", int, bytes=1)  # message type
 ]
 
 
-
-request = [  #mtype: 1
-    Field("request",str,255),
+request = [  # mtype: 1
+    Field("request", str, 255),
 ]
-response = [ #mtype: 2
-    Field("response",str,255),
+response = [  # mtype: 2
+    Field("response", str, 255),
 ]
-
 
 
 standard_header_format = Format(standard_header)
-
